9eddc26b4b3cc5ed66db93a5e3f6cc6a79d41cae
Wael M Haidar <wah218@lehigh.edu>
2014-08-27 16:35:41 +0000

    updated file

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//Wael Haidar
+//Lab 1: HelloWorld 27/8/14 12:18
+//CSE2
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        System.out.println("O                           O");
+        System.out.println("O    Hello World 2014       O");
+        System.out.println("O                           O");
+        System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+
+        
+    }
+}
